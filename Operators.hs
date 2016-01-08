da95edf4a857524487f70c09c2eb420f77372ce9
Jeffrey Lyman <JeffreyTLyman@gmail.com>
2016-01-08 17:13:26 -0600

    unified model with the theory

--- Operators.hs
+++ Operators.hs
@@ -17,39 +17,30 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Maybe (fromMaybe)
 
-data Statement where
-    Assign  :: Decons -> Expr -> Statement
-    StExpr  :: Expr -> Statement
-    StFun   :: Function Statement -> Statement
+type Id = String
 
-data Decons where
-    DName :: String -> Decons
-    DCons :: String -> [Decons] -> Decons
-    DNil  :: Decons
+data Program = Program { 
+      classes   :: [Class], 
+      functions :: [Expr], 
+      datatypes :: [Data]
+    }
+    -- todo type aliases and error types
+
+data Pat where
+    DName :: Id -> Pat
+    DCons :: Id -> [Pat] -> Pat
+    DNil  :: Pat
 
 data Expr  where
     ELit    :: Literal -> Expr
+    EVar    :: Id -> Expr
+    ELambda :: Lambda Expr -> Expr
     EFCall  :: Call -> Expr
-    EVar    :: String -> Expr
-    ECons   :: Call -> Expr
-    OAdd    :: Expr -> Expr -> Expr
-    OSub    :: Expr -> Expr -> Expr
-    OMult   :: Expr -> Expr -> Expr
-    ODiv    :: Expr -> Expr -> Expr
-    OIntDiv :: Expr -> Expr -> Expr
-    OMod    :: Expr -> Expr -> Expr
-    OEq     :: Expr -> Expr -> Expr
-    OLT     :: Expr -> Expr -> Expr
-    OLTE    :: Expr -> Expr -> Expr
-    OGT     :: Expr -> Expr -> Expr
-    OGTE    :: Expr -> Expr -> Expr
-    OOr     :: Expr -> Expr -> Expr
-    OAnd    :: Expr -> Expr -> Expr
-    OXor    :: Expr -> Expr -> Expr
-    ONot    :: Expr -> Expr
-    ONeg    :: Expr -> Expr
-    OPos    :: Expr -> Expr
-    
+    ECons   :: Call -> Expr --special case of function application
+    EFix    :: Id -> Lambda Expr -> Expr
+    ELet    :: Pat -> Expr -> Expr -- EChain ~= in
+    ECase   :: Expr -> [(Pat, Expr)] -> Expr
+    EChain  :: Expr -> Expr -> Expr
 
 data Call = Call {
     fname :: String,
@@ -70,7 +61,7 @@ call cons np = cons <$> do
 fcall    = call EFCall miLName
 consCall = call ECons  miUName
 
-assign = Assign <$> decons <*> (miString "=" *> expr)
+elet = ELet <$> decons <*> (miString "=" *> expr)
 
 decons = name' <|> dec' <|> nil' <|> parens decons
     where dec' = DCons <$> miUName <*> parens (decons `sepBy` comma')
@@ -81,66 +72,32 @@ term = parens expr <|> eLiteral <|> var <|> fcall <|> consCall
 
 var = EVar <$> miLName
 
-expr = makeExprParser term table <?> "expression"
+expr = makeExprParser term operators <?> "expression"
 
 miLiteral = miLexeme (literal expr)
 eLiteral = ELit <$> miLiteral
-table = [[oPos, oNeg],
-         [oMult, oDiv, oIntDiv, oMod],
-         [oAdd, oSub],
-         [oEq, oLT, oLTE, oGT, oGTE],
-         [oNot],
-         [oAnd, oOr, oXor]]
+operators = [[miuOp "+", miuOp "-"],
+         [mibOp "*", mibOp "/", mibOp "//", mibOp "%"],
+         [mibOp "+", mibOp "%"],
+         [mibOp "==", mibOp "<", mibOp "<=", mibOp ">", mibOp ">="],
+         [iuOp "not"],
+         [ibOp "and", ibOp "or", ibOp "xor"]]
 
+bOp p s = InfixL (p s *> pure (\a b -> EFCall $ Call s [a,b] []))
+uOp s = Prefix (p s *> pure (\a -> EFCall $ Call s [a] []))
+mibOp = bOp miString 
+ibOp = bOp iString 
+miuOp = uOp miString 
+iuOp = uOp iString 
 
-miExpr cstr s = InfixL (miString s *> pure cstr)
-iExpr cstr s = InfixL (iString s *> pure cstr)
+{-instance Show Expr where-}
+    {-show (ELit l) = show l-}
+    {-show (EFCall f) = show f-}
 
-oAdd = miExpr OAdd "+"
-oSub = miExpr OSub "-"
-oMult = miExpr OMult "*"
-oDiv = miExpr ODiv "/"
-oIntDiv = miExpr OIntDiv "//"
-oMod = miExpr OMod "%"
-oEq = miExpr OEq "=="
-oLT = miExpr OLT "<"
-oLTE = miExpr OLTE "<="
-oGT = miExpr OGT ">"
-oGTE = miExpr OGTE "<="
+{-instance Show Statement where-}
+    {-show (Assign n e) = "assign " ++ show n ++ " " ++ show e-}
 
-oAnd = iExpr OAnd "and"
-oOr = iExpr OOr "or"
-oXor = iExpr OXor "xor"
-
-oNot = Prefix (iString "not" *> pure ONot)
-oNeg = Prefix (miString "-" *> pure ONeg)
-oPos = Prefix (miString "+" *> pure OPos)
-
-instance Show Expr where
-    show (OAdd a b) = "(" ++ show a ++ "+" ++ show b ++ ")"
-    show (OSub a b) = "(" ++ show a ++ "-" ++ show b ++ ")"
-    show (OMult a b) = "(" ++ show a ++ "*" ++ show b ++ ")"
-    show (ODiv a b) = "(" ++ show a  ++ "/" ++ show b ++ ")"
-    show (OIntDiv a b) = "(" ++ show a ++ "//" ++ show b ++ ")"
-    show (OMod a b) = "(" ++ show a ++ "%" ++ show b ++ ")"
-    show (OEq a b) = "(" ++ show a ++ "==" ++ show b ++ ")"
-    show (OLT a b) = "(" ++ show a ++ "<" ++ show b ++ ")"
-    show (OLTE a b) = "(" ++ show a ++ "<=" ++ show b ++ ")"
-    show (OGT a b) = "(" ++ show a ++ ">" ++ show b ++ ")"
-    show (OGTE a b) = "(" ++ show a ++ ">=" ++ show b ++ ")"
-    show (OOr a b) = "(" ++ show a ++ " or " ++ show b ++ ")"
-    show (OAnd a b) = "(" ++ show a ++ " and " ++ show b ++ ")"
-    show (OXor a b) = "(" ++ show a ++ " xor " ++ show b ++ ")"
-    show (ONot a) = "(not " ++ show a ++ ")"
-    show (ONeg a) = "(-" ++ show a ++ ")"
-    show (OPos a) = "(+" ++ show a ++ ")"
-    show (ELit l) = show l
-    show (EFCall f) = show f
-
-instance Show Statement where
-    show (Assign n e) = "assign " ++ show n ++ " " ++ show e
-
-instance Show Decons where
+instance Show Pat where
     show (DCons n as) = n ++ intercalate "," (map show as)
     show (DName n) = n
 
