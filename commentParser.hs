87b1fb1c8140af57d0e05f9439e17c334444f799
Jeffrey Lyman <jlyman@macalester.edu>
2016-01-25 14:19:44 -0600

    cleaned out intermediary files

--- commentParser.hs
+++
@@ -1,8 +0,0 @@
-import Text.ParserCombinators.Parsec
-
-codeFile = endBy line myEOL
-line = sepBy nonBreakSpace  $ many (name <|> continuation)
-continuation = char '\\' skipMany nonBreakSpace (char '\n')
-
-nonBreakSpace = '\t' <|> ' '
-name = many (alphaNum <|> char '_')
